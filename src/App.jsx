9b02cf6b31373cf6174962c7c13360b5f536ea25
Luis Eduardo Uscanga Serrano <isc18350503@gmail.com>
2022-05-25 10:03:07 -0500

    Se realizaron los primeros cambios al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import './App.css';
 
 function App() {
+  const opciones = async()=>{
+    const respuesta = await 
+    fetch("https://api.genshin.dev/")
+    const types = respuesta.json()
+    console.log({types});  
+  };
+
+  opciones();
+
   return (
-    <div className='App card'>
-    <h3>Prueba texto</h3>
+    <div className='App container'>
+    <h3>Genshin Impact Dex</h3>
+    <select>
+    <option value="">Seleccione un elemento</option>
+      <option value="artifacts"></option>
+      <option value="boss"></option>
+      <option value="consumables"></option>
+      <option value="domains"></option>
+      <option value="elements"></option>
+      <option value="enemies"></option>
+      <option value="materials"></option>
+      <option value="nations"></option>
+      <option value="weapons"></option>
+    </select>
     </div>
   );
 }
